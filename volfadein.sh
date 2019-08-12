464f692366a48cb02bc20e3d90386bd86d0a2e9b
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-12 10:50:18 +0200

    Update volfadein.sh

--- volfadein.sh
+++ volfadein.sh
@@ -2,7 +2,7 @@
 # script to use locally, to fade in after startup. use with "crontab -e" : "@reboot /home/volumio/volfadein.sh"
 # don't forget to "sudo chmod +x volfadein.sh" 
 
-x=0
+x=5
 while [ $x -le 100 ]
 do
 sleep 3s
