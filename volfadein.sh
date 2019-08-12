fef5e478251ded4feeb2da668d5739b33976448f
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-12 13:50:28 +0200

    Update volfadein.sh

--- volfadein.sh
+++ volfadein.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # script to use locally, to fade in after startup. use with "crontab -e" : "@reboot /home/volumio/volfadein.sh"
 # don't forget to "sudo chmod +x volfadein.sh" 
+sleep 30s
+/volumio/app/plugins/system_controller/volumio_command_line_client/volumio.sh play
 
 x=25
 while [ $x -le 100 ]
