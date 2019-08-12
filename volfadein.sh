c3b7b79fe5f3e40bcab0e05620a8a1bf16be1070
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-12 10:47:44 +0200

    Update volfadein.sh

--- volfadein.sh
+++ volfadein.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# script to use locally, to fade in after startup. use with crontab -e: @reboot volfadein.sh
+# script to use locally, to fade in after startup. use with "crontab -e" : "@reboot /home/volumio/volfadein.sh"
 # don't forget to "sudo chmod +x volfadein.sh" 
 
 x=0
