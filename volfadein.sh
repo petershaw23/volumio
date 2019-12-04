81ab035b8ff6eb0b258d7bc59f668182a81f1f82
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-12-04 09:04:55 +0100

    Update volfadein.sh

--- volfadein.sh
+++ volfadein.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # script to use locally, to autostart+fade in after startup. use with "crontab -e" : "@reboot /home/volumio/volfadein.sh"
 # don't forget to "sudo chmod +x volfadein.sh" 
-sleep 37s
+sleep 38s
 /volumio/app/plugins/system_controller/volumio_command_line_client/volumio.sh next
-
-x=24
+sudo systemctl restart snapserver
+x=20
 while [ $x -le 100 ]
 do
-sleep 3s
+sleep 3.5s
 echo "set value x to: $x"
 /volumio/app/plugins/system_controller/volumio_command_line_client/volumio.sh volume +$x
 x=$(( $x + 2 ))
