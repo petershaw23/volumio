fe5fa4e09aeca2ee928f058ba3de024a14345691
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-12-28 14:49:00 +0100

    Create snapfade.py

---
+++ snapfade.py
@@ -0,0 +1,22 @@
+import asyncio
+import snapcast.control
+from time import sleep      # Importing the time library to provide the delays in program
+sleep (35)
+loop = asyncio.get_event_loop()
+server = loop.run_until_complete(snapcast.control.create_server(loop, 'localhost')) #snapcast server IP
+
+# print all client names
+#for client in server.clients:
+#  print(client.friendly_name)
+#  print(client.identifier)
+#  print(client.group)
+
+
+
+#client = server.clients[0] # set volume for client #0 to 50%
+client = 'b8:27:eb:b2:5f:83' #this is the client ID of the client you want to control
+
+
+for (int i = 0; i < 5; ++i)
+loop.run_until_complete(server.client_volume(client, {'percent': 2, 'muted': False}))
+sleep (4)
