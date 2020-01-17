3f4ad22d6f372a5449fe34a27cc16a891f505106
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-01-17 10:59:28 +0100

    Update snapcastvol.py

--- snapcastvol.py
+++ snapcastvol.py
@@ -5,15 +5,14 @@ sleep (35)
 loop = asyncio.get_event_loop()
 server = loop.run_until_complete(snapcast.control.create_server(loop, 'localhost')) #snapcast server IP
 
-# print all client names
+# print all client names. do this first to find out client IDs. you can also do this with the android app snapcontrol or by just finding out the mac address of the client
+
 #for client in server.clients:
 #  print(client.friendly_name)
 #  print(client.identifier)
 #  print(client.group)
 
 
-
-#client = server.clients[0] # set volume for client #0 to 50%
 client = 'b8:27:eb:b2:5f:83' #this is the client ID of the client you want to control
 loop.run_until_complete(server.client_volume(client, {'percent': 2, 'muted': False}))
 sleep (4)
